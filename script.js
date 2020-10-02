8bb0063e70c5a3044e83f18608541833a44fa1a4
Favour Maryjane <favourmje@gmail.com>
2020-10-02 13:00:04 +0100

    adding metal data

--- script.js
+++ script.js
@@ -19,3 +19,19 @@ resetbtn.addEventListener('click', function(){
     firstPage.classList.remove('hidden');
     thirdPage.classList.add('hidden');
 })
+
+function Ageclat(){
+    var name = document.getElementById("fname").value;
+    var name1 = document.getElementById("lname").value;
+    var age = document.getElementById("agebox").value;
+
+    let nam = document.getElementById("span1");
+    nam.textContent = name;
+    let nam1 = document.getElementById("span2");
+    nam1.textContent = name1;
+    let nam2;
+    nam2 = age * 365 
+    nam3 = document.getElementById("span3");
+    nam3.textContent = nam2;
+}
+
