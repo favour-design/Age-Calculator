29b983d3ff554416a752b8e99f574221b0508d66
Favour Maryjane <favourmje@gmail.com>
2020-10-02 15:04:09 +0100

    adding metal data

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Age-Calculator
 This is an app used in calculating the number of years lived by a person
+## live demo
+[see project live here]( https://github.com/favour-design/Age-Calculator.git)
--- script.js
+++ script.js
@@ -1,51 +1,47 @@
-const button1 = document.querySelector('.me');
-const secondPage = document.querySelector('.child2');
-const firstPage = document.querySelector('.child1');
-const submitBtn = document.querySelector('.NaNs');
-const thirdPage = document.querySelector('.child3');
+let button1 = document.querySelector('.me');
+let secondPage = document.querySelector('.child2');
+let firstPage = document.querySelector('.child1');
+let submitBtn = document.querySelector('.NaNs');
+let thirdPage = document.querySelector('.child3');
 console.log(button1)
-const resetbtn = document.querySelector('.Re');
+let resetbtn = document.querySelector('.Re');
 
 button1.addEventListener('click',function(){
     secondPage.classList.remove('hidden');
     firstPage.classList.add('hidden');
 })
 
-submitBtn.addEventListener('click', function(){
-    thirdPage.classList.remove('hidden');
-    secondPage.classList.add('hidden');
-})
+// submitBtn.addEventListener('click', function(){
+// })
 resetbtn.addEventListener('click', function(){
     firstPage.classList.remove('hidden');
     thirdPage.classList.add('hidden');
 })
 
 function Ageclat(){
-    var name = document.getElementById("fname").value;
-    var name1 = document.getElementById("lname").value;
-    var age = document.getElementById("agebox").value;
+    thirdPage.classList.remove('hidden');
+    secondPage.classList.add('hidden');
+    var name, name1, age;
+    name = document.getElementById("fname").value;
+    name1 = document.getElementById("lname").value;
+    age = document.getElementById("agebox").value;
 
-    let nam = document.getElementById("span1");
-    nam.textContent = name;
-    let nam4 = document.getElementById("span4");
-    nam4.textContent = name1;
-    let nam1 = document.getElementById("span2");
-    nam1.textContent = age;
-    let nam2;
-    nam2 = age * 365 
-    nam3 = document.getElementById("span3");
-    nam3.textContent = nam2;
-
-    if(name === "" || name1 ===""){
+    if(name === "" || name1 ==="" || age ==="" ){
         alert("Fill in the missing details!");
-          return;
-    } else if (age ==="" || name ===""){
-        alert("Form must be filled Properly");
-      return;
-    }   else if (age ==="" || name1 ===""){ 
-        alert("Your Details Are Not Complete");
         return;
-      
+    }   else {
+            let nam = document.getElementById("span1");
+            nam.textContent = name;
+            let nam4 = document.getElementById("span4");
+            nam4.textContent = name1;
+            let nam1 = document.getElementById("span2");
+            nam1.textContent = age;
+            let nam2;
+            nam2 = age * 365 
+            nam3 = document.getElementById("span3");
+            nam3.textContent = nam2;
     }
+    
+
 }
 
